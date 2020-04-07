cdf125222d1dfe3a0bed3ce1b9b2d1703ece348b
Anjana Nair <anju740@gmail.com>
2020-04-07 00:49:16 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,7 +3,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
     create_table :candies do |t|
       t.string :name
-      t.float :price
+      t.string :location
       t.string :image_url
       t.string :size
       t.timestamps
